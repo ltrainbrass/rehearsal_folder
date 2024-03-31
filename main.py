b0956551f1213ed3ed058301685000409dde47f4
Ryan Cohen <ryjco20@gmail.com>
2024-03-31 00:41:36 -0400

    Add option to read from only first table in agenda

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ def get_folders():
     folders = []
 
     soup = BeautifulSoup(html, 'html.parser')
+    if read_first_table:
+        soup = soup.select_one('table')
     for link in soup.find_all('a', href=True):
         href = link['href']
 
@@ -124,12 +126,14 @@ def copy_agenda_files():
 if __name__ == '__main__':
     parser = ArgumentParser(description='Copies files from linked directories in a Google Drive agenda file to an output folder')
     parser.add_argument('config_ini_file', help='the file containing the configuration for the app', nargs=1)
+    parser.add_argument('--first-table', action='store_true', help='only the linked directories in the first table of the agenda file should be read')
     args = parser.parse_args()
 
     config_ini_file = args.config_ini_file
     config = configparser.RawConfigParser()
     config.read(config_ini_file)
     file_id = config['agenda_file']['id']
+    read_first_table = args.first_table
 
     service = create_service()
     html = service.files().export_media(fileId=file_id, mimeType='text/html').execute()
